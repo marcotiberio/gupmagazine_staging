1ff9234acfece1f98bd0a27326788173b4d88972
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-08-24 16:51:26 +0200

    added buttons

--- landing-page-special.php
+++ landing-page-special.php
@@ -24,9 +24,13 @@
 		<div class="intro">
 			<img id="introCover" src="https://gupmagazine.com/wp-content/uploads/2020/07/66gupbanner.png" alt="" />
 			<h2 id="introText">GUP #66 presents a limited edition of 66 issues with an original signed print of the
-				Dutch photographer Justine Tjallinks only for €100! Pick and order your desired numbered issue now by
-				clicking on the number you like most hereunder!
+				Dutch photographer Justine Tjallinks only for €100,-. Pick and order your desired numbered issue now by
+				clicking on the GUP webshop. Hurry up!
 			</h2>
+			<div class="landing-buttons">
+				<button id="buynowButton">Order now</button>	
+				<button id="websiteButton">Go to website</button>	
+			</div>
 		</div>
 	</div>
 
