068e3fc7a32666070f48a9cd0f3f05599ee7ee1a
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-08-24 16:35:19 +0200

    reordered template

--- landing-page-special.php
+++ landing-page-special.php
@@ -21,11 +21,12 @@
 	?>
 
 	<div>
-		<div class="intro"><img id="introCover" src="https://gupmagazine.com/wp-content/uploads/2020/07/66gupbanner.png"
-				alt="" />
+		<div class="intro">
+			<img id="introCover" src="https://gupmagazine.com/wp-content/uploads/2020/07/66gupbanner.png" alt="" />
 			<h2 id="introText">GUP #66 presents a limited edition of 66 issues with an original signed print of the
 				Dutch photographer Justine Tjallinks only for €100! Pick and order your desired numbered issue now by
-				clicking on the number you like most hereunder!</h2>
+				clicking on the number you like most hereunder!
+			</h2>
 		</div>
 	</div>
 
